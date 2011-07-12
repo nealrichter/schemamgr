575dd89e299490f4c4e33c14107422b6f994ccb9
Neal Richter <nealrichter>
2011-07-12 15:32:32 -0600

    better example create objects file

--- build/create_X_objects_v1_20110615.sql
+++ build/create_X_objects_v1_20110615.sql
@@ -1,26 +1,10 @@
 
--- users  
-CREATE TABLE anon_user (
-  user_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  cookie VARCHAR(128),
-  dt_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-  PRIMARY KEY (user_id),
-
- INDEX idx_au_cookie(cookie)
-)
-ENGINE = InnoDB
-CHARACTER SET utf8 COLLATE utf8_general_ci;
-
--- keywords  
-CREATE TABLE keyword (
-  keyword_id INTEGER UNSIGNED NOT NULL,
-  value VARCHAR(128) NOT NULL,
-  status INTEGER UNSIGNED NOT NULL default 1,
-  quality_score INTEGER UNSIGNED NOT NULL default 0,
-  PRIMARY KEY (keyword_id),
-
- INDEX idx_k_value (value)
+-- example
+CREATE TABLE example (
+  id INTEGER,
+  data VARCHAR(100),
+  
+  PRIMARY KEY (id)
 )
 ENGINE = InnoDB
 CHARACTER SET utf8 COLLATE utf8_general_ci;
